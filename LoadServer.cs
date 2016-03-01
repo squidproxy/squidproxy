2beb1d8f7df9e818e685fdfafc32c6c7a80a3af3
badoutech2017@gmail.com <badoutech2017@gmail.com>
2016-03-01 21:34:52 +0800

    project file

---
+++ LoadServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BadouSquidClient
+{
+    class LoadServer
+    {
+        public string ReadINI(string section, string key, string fileName)
+        {
+            StringBuilder temp = new StringBuilder(1024);
+        //    GetPrivateProfileString(section, key, "NullValue", temp, 1024, fileName);
+            return temp.ToString();
+        }
+
+        public void UpdateMenu()
+
+        {
+
+            //添加菜单一 
+       //     ToolStripMenuItem subItem;
+       //     subItem = AddContextMenu("服务器", contextMenuStrip1.Items, null);
+
+
+
+            string ServerindexString = ReadINI("DefaultStartup", "index", "F:\\C#\\SquidClient巴豆\\pac\\pac\\bin\\Release\\SquidConfig.ini");
+
+      //      MessageBox.Show(ServerindexString);
+
+            int ServerindexInt = int.Parse(ServerindexString);
+
+            int j = 0;
+
+            for (int i = 0; i < ServerindexInt; i++)
+            {
+
+
+                j = j + 1;
+                string ServerAddress = ReadINI("User", "Server" + j.ToString(), "F:\\C#\\SquidClient巴豆\\pac\\pac\\bin\\Release\\SquidConfig.ini");
+       
+                //    MessageBox.Show(ServerAddress);
+
+                //   AddContextMenu(ServerAddress, subItem.DropDownItems, new EventHandler(MenuClicked));
+
+
+
+            }
+
+        //    AddContextMenu("编辑服务器", subItem.DropDownItems, new EventHandler(EditServer));
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pac
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //http://stackoverflow.com/questions/6486195/ensuring-only-one-application-instance
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "UniqueAppId", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("BadouSquidClient客户端已经运行!", "提示");
+                return;
+            }
+
+            Application.Run(new Form1());
+
+            GC.KeepAlive(mutex);                // mutex shouldn't be released - important line
+        }
+    }
+}
